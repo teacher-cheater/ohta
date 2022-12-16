43bded50aa1f6f49b10a90a8f58fff8e5208a4c0
teacher_cheater <johnnykayfun@gmail.com>
2022-12-16 12:51:40 +0300

    edit slider, fix bugs

--- js/app.js
+++ js/app.js
@@ -1,17 +1,17 @@
-let slideIndex = 0;
+//let slideIndex = 0;
 
-function showSlides() {
-  let i;
-  const slides = document.querySelectorAll(".hover-slider__image");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.opacity = "0";
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {
-    slideIndex = 1;
-  }
-  slides[slideIndex - 1].style.opacity = "1";
-  setTimeout(showSlides, 2000);
-}
+//function showSlides() {
+//  let i;
+//  const slides = document.querySelectorAll(".hover-slider__image");
+//  for (i = 0; i < slides.length; i++) {
+//    slides[i].style.opacity = "0";
+//  }
+//  slideIndex++;
+//  if (slideIndex > slides.length) {
+//    slideIndex = 1;
+//  }
+//  slides[slideIndex - 1].style.opacity = "1";
+//  setTimeout(showSlides, 2000);
+//}
 
-showSlides();
+//showSlides();
